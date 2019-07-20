bcb2b7472cff643854b4bf40e37d00aa749a5372
tomporvaz <tomporvaz@gmail.com>
2019-07-19 20:09:50 -0400

    Added sychonous bcrypt.

--- server.js
+++ server.js
@@ -24,8 +24,10 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
-
-
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
